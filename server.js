11cbfd6bba2f460cbfdd105adf512510a7df09e2
Alexandra Voit <145194484+alexandravoit@users.noreply.github.com>
2026-03-01 11:59:37 +0200

    Preparation for initial deployment

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import express from "express";
+import cors from "cors";
 import courseRoutes from "./src/routes/courses.js";
 import moduleRoutes from "./src/routes/modules.js";
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
+
+app.use(cors({
+    origin: [
+        'https://bachlog.vercel.app',
+        'http://localhost:3000'
+    ],
+    credentials: true
+}));
 
 app.use(express.json());
 
